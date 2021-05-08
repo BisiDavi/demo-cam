f78b6f4f03e23e0e7f29dd0492edb6e28a3421c8
BisiDavi <oludavidconnect@gmail.com>
2021-05-08 16:47:30 +0100

    signup page

---
+++ pages/signup.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container is-fluid is-flex is-justify-content-center">
+    <section class="signup"></section>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.signup {
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 40px;
+}
+</style>
